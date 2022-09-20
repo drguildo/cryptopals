e74ea9c879c167599eba4784a41efa36d996ef4e
Simon Morgan <sjm@sjm.io>
2022-09-20 18:11:46 +0100

    Add previous challenges to command output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use cryptopals::set1::single_byte_xor_cypher;
+use cryptopals::set1::{hex_to_bytes, single_byte_xor_cypher};
 
 fn main() {
+    println!("== Set 1, Challenge 1 ==");
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let bytes = cryptopals::set1::hex_to_bytes(&hex);
+    let base64 = cryptopals::set1::bytes_to_base64(&bytes);
+    println!("{} = {}", hex, base64);
+
+    println!("== Set 1, Challenge 2 ==");
+    let a = "1c0111001f010100061a024b53535009181c";
+    let b = "686974207468652062756c6c277320657965";
+    let xored = cryptopals::set1::xor_buffers(&hex_to_bytes(a), &hex_to_bytes(b));
+    println!("{} ^ {} = {}", a, b, cryptopals::set1::bytes_to_hex(&xored));
+
     println!("== Set 1, Challenge 3 ==");
     let hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
     if let Some(candidate_key) = single_byte_xor_cypher(&hex) {
