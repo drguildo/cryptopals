071d3e9f9e7afd87835ddbb16e0a437052fb13f6
Simon Morgan <sjm@sjm.io>
2022-09-23 22:34:15 +0100

    Remove redundant to_string call

--- src/set1.rs
+++ src/set1.rs
@@ -27,7 +27,7 @@ pub fn hex_to_bytes(hex: &str) -> Vec<u8> {
 pub fn bytes_to_hex(bytes: &[u8]) -> String {
     bytes
         .iter()
-        .map(|b| format!("{:02x}", b).to_string())
+        .map(|b| format!("{:02x}", b))
         .collect::<Vec<String>>()
         .join("")
 }
