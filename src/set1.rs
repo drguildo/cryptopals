0c87cda0dcfa0b98e4efa163c4f17cc6c7d2cc85
Simon Morgan <sjm@sjm.io>
2022-10-02 15:03:55 +0100

    Add a function for transposing a slice

--- src/set1.rs
+++ src/set1.rs
@@ -164,6 +164,26 @@ fn find_keysize_average(encrypted: &[u8]) -> u8 {
     keysize_distance.unwrap().0
 }
 
+// TODO: Make this more declarative/functional
+fn transpose(bytes: &[u8], block_size: usize) -> Vec<Vec<u8>> {
+    let chunks = bytes.chunks(block_size);
+    let mut transposed: Vec<Vec<u8>> = Vec::new();
+    for chunk in chunks {
+        for i in 0..block_size {
+            if i < chunk.len() {
+                if let Some(block) = transposed.get_mut(i) {
+                    block.push(chunk[i]);
+                } else {
+                    let mut block = Vec::new();
+                    block.push(chunk[i]);
+                    transposed.insert(i, block);
+                }
+            }
+        }
+    }
+    transposed
+}
+
 pub fn find_repeating_key_xored_string(encrypted: &[u8]) -> Option<Candidate> {
     let keysize = find_keysize_simple(encrypted);
     println!("{}", keysize);
@@ -253,5 +273,31 @@ mod test {
             crate::set1::hamming_distance("this is a test".as_bytes(), "wokka wokka!!!".as_bytes()),
             37
         );
+    }
+
+    #[test]
+    fn transpose_empty() {
+        let transposed = crate::set1::transpose(&[], 4);
+        assert!(transposed.is_empty());
+    }
+
+    #[test]
+    fn transpose_equally_sized() {
+        let bytes: Vec<u8> = (0..12).collect();
+        let transposed = crate::set1::transpose(&bytes, 4);
+        assert_eq!([0, 4, 8].to_vec(), transposed[0]);
+        assert_eq!([1, 5, 9].to_vec(), transposed[1]);
+        assert_eq!([2, 6, 10].to_vec(), transposed[2]);
+        assert_eq!([3, 7, 11].to_vec(), transposed[3]);
+    }
+
+    #[test]
+    fn transpose_unequally_sized() {
+        let bytes: Vec<u8> = (0..10).collect();
+        let transposed = crate::set1::transpose(&bytes, 4);
+        assert_eq!([0, 4, 8].to_vec(), transposed[0]);
+        assert_eq!([1, 5, 9].to_vec(), transposed[1]);
+        assert_eq!([2, 6].to_vec(), transposed[2]);
+        assert_eq!([3, 7].to_vec(), transposed[3]);
     }
 }
